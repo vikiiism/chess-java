aefb0144504bcf2a1f13801f0866c56afb5367b7
vikiiism <vikiiism@mail.bg>
2020-04-06 00:13:21 +0300

    Modified package and main class

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,13 @@
 
+import GameSettings.StartMenuEngine;
+
 import javax.swing.*;
 
 public class Game implements Runnable {
     public void run() {
-        SwingUtilities.invokeLater(new StartMenu());
+        SwingUtilities.invokeLater(new StartMenuEngine());
     }
-    
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Game());
     }
